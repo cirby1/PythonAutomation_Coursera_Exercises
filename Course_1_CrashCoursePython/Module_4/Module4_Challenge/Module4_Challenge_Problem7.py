a35a59014619fda69fe00860fc3430ccc6e215c7
cirby <calvin.irby01@gmail.com>
2025-12-05 15:16:28 -0600

    Still working on Problem 7.

---
+++ Course_1_CrashCoursePython/Module_4/Module4_Challenge/Module4_Challenge_Problem7.py
@@ -0,0 +1,29 @@
+"""
+Complete the function so that input like "This is a sentence." will return a dictionary that holds the count of each letter that occurs in the string.
+
+1. Accept a string "text" variable through the function's parameters.
+
+2. Iterate over each character the input string to count the frequency of each letter found, do not count spaces, numbers or punctuation. 
+
+3. Populate the new dictionary with the letters as keys, ensuring each key is unique, and assign the value for each key with the count of that letter. 
+
+4. Return the dictionary.
+"""
+
+#def letterCounter(input_sentence):
+#letterCountDictionary = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I":  0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0, "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h" = 0, "i" = 0, "j" = 0, "k" = 0, "l" = 0, "m" = 0, "n" = 0, "o" = 0, "p" = 0, "q" = 0, "r" = 0, "s" = 0, "t" = 0, "u" = 0, "v" = 0, "w" = 0, "x" = 0, "y" = 0, "z" = 0}
+
+#if "A" in letterCountDictionary:
+#    print("True")
+
+"""
+    for individual_character in range(0, len(input_sentence)):
+        print(input_sentence[individual_character])
+        if input_sentence[individual_character].isalpha():
+            letterCountDictionary.update 
+
+    print(input_sentence.split()) 
+
+letterCounter("This is a sentence.")
+"""
+
