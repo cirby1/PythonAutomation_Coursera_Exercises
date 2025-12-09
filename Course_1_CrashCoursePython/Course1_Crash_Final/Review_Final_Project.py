5b0dc0981e11f02fd02eb4ed89099c0e1abe7ce1
cirby <calvin.irby01@gmail.com>
2025-12-09 12:31:42 -0600

    Gamer Activity Project Start.

---
+++ Course_1_CrashCoursePython/Course1_Crash_Final/Review_Final_Project.py
@@ -0,0 +1,19 @@
+"""
+Okay I'm going to do the final project again, but this time with a little bit of gaming zeal. 
+This is going to be a gamer tracker for online. I wanna make a program that will keep track of all the gamers that are currently online. 
+
+But first I will need a task list of things to make. I need:
+-A class to hold the gamers date of activity (both calendar and time all in one string, "type of activity whether they logged in or out", "type of console used for gaming", and their gamertag. 
+
+-I will need to create all the gamer activity and store it into a list of objects. And each object will create the activity being tracked. 
+
+- I need a function to grab the event date. 
+
+-A need a function to tell me the current users online, this is the cream of the crop. It will have to sort the list of gamer activity by date, that way there's no confusion. Make a dictionary that will track current users. And then go through that entire gamer activity list and keep on adding/removing based on when they login/logout. At the end the gamers still logged in will remain. 
+
+-At the end I will need to generate a report of each console and the users that are logged in for that console. I will need to print this line by line.
+
+"""
+
+
+
--- Course_1_CrashCoursePython/Course1_Crash_Final/Review_MyOwnWork.py
+++ Course_1_CrashCoursePython/Course1_Crash_Final/Review_MyOwnWork.py
@@ -27,15 +27,17 @@ class Event:
         print(self.username)
 
 #Okay, I was able to get the Event Class going. 
-myEvent = Event("12-02-2025 10:28", "Login", "Workstation.local", "Bobby")
-myEvent.printEventReport()
+#myEvent = Event("12-02-2025 10:28", "Login", "Workstation.local", "Bobby")
+#myEvent.printEventReport()
 
 #Let's make some Events to cycle through:
 events = [
 Event("12-09-2025 10:29:05", "Login", "workstation.local", "Bobby"),
 Event("12-09-2025 10:30:44", "Logut", "workstation.local", "Bobby"),
 Event("12-10-2025, 12:00:02", "Login", "webserver.local", "Jim"),
-Event("12-11-2025, 12:00:03", "Login", "mailserver.local", "Richard")
+Event("12-10-2025, 12:00:02", "Logout", "webserver.local", "Jim"),
+Event("12-11-2025, 12:00:03", "Login", "mailserver.local", "Richard"),
+Event("12-12-2025, 12:01:04", "Login", "mailserver.local", "Sally")
 ]
 
 #We need a function that grabs the date from the Event object.
@@ -44,6 +46,36 @@ def getDate(input_event):
     #The output is going to the attribute for this event which is the date. 
     return input_event.date
 
+#Okayyyyy, now we are onto the hard part. We need to make a fancy pants function here. One that, I forgot, let me yank it with Vim...
 
+#-Function that returns the machines in use. It should create a machines dictionary, and if there's a login, add that machine to the dictionary. If there's a logout, it should remove that user from the dictionary. You can use the .add and input the events.machine object/attribute. That way you have a current list of machines. 
 
+#Okay, so I need to make a dictionary that keeps track of the machines that are being used. The thing is, I don't know what it should input. All the events? Unless I get a nother function just to extract the name and machine. 
+#This doesn't really make sense. Let me check...
+def machinesUsed(input_Events):
+    #I need to sort the events. 
+    input_Events.sort(key=getDate)
+    machines_in_use = {}
+    for event in input_Events:
+        #Here I need to do some logic. If the Event is a login. It gets put into the dictionary, If not then it gets removed. Otherwise, If there is no record of this machine is not in the machines list. Then start a blank set so that we can start keeping record. 
+        if event.machine not in machines_in_use:
+            #So here is the first check to see if we need to start an empty set for machines in use for that machine. 
+            machines_in_use[event.machine] = set()
+        if event.type == "Login":
+            machines_in_use[event.machine].add(event.username)
+        elif event.type == "Logout":
+            machines_in_use[event.machine].remove(event.username)
+    print(machines_in_use)
+    return machines_in_use
+
+def generate_report(machines):
+    for machine, users in machines.items():
+        if len(users) > 0:
+            user_list = ", ".join(users)
+            print("{} {}".format(machine, user_list))
+
+users = machinesUsed(events)
+print(users)
+
+generate_report(users)
 
