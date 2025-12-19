2633920fe86b4aa4d59a882a4feabc30480eb0c7
cirby <calvin.irby01@gmail.com>
2025-12-19 10:18:23 -0600

    -

--- Course_2_ProgrammingPython_OS/Module3/Review_CapturingGroups/video_rearrangename.py
+++ Course_2_ProgrammingPython_OS/Module3/Review_CapturingGroups/video_rearrangename.py
@@ -1,8 +1,15 @@
 #Fix the regular Expression used in the rearrange function so that it matches middle names, middle initials, as well as double surnames.
 
+#import re
+#This line finally works! I was able to get it to be accepted by the video exercise.
+#print(re.search(r"^([A-Z]\w*),\s([A-Z][\w\.\-]*)\s?([A-Z][\w\.\-]*)$",  "Kennedy, John-F."))
+
 import re
+def rearrange_name(name):
+    result = re.search(r"([A-Z]\w*),\s([A-Z][\w\.\-]*)\s?([A-Z][\w\.\-]*)$", name)
+    if result == None:
 
-
-print(re.search(r"^([A-Z]\w*),\s([A-Z][\w\.\-]*)\s?([A-Z][\w\.\-]*)$",  "Kennedy, John-F."))
+name=rearrange_name("Kennedy, John F")
+print(name)
 
 
