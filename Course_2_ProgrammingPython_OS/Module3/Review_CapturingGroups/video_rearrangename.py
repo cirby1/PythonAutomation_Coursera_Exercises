42c7a4065637ba361e337b47b5964624439f5f33
cirby <calvin.irby01@gmail.com>
2025-12-19 10:01:32 -0600

    Check point - checking middle names for video exercise

--- Course_2_ProgrammingPython_OS/Module3/Review_CapturingGroups/video_rearrangename.py
+++ Course_2_ProgrammingPython_OS/Module3/Review_CapturingGroups/video_rearrangename.py
@@ -1,12 +1,8 @@
 #Fix the regular Expression used in the rearrange function so that it matches middle names, middle initials, as well as double surnames.
 
 import re
-def rearrange_names(name):
-    result = re.search(r"^(\w)*, (\w)*$", name)
-    if result == None:
-        return name
-    return "{} {}".format(result[2], result[1])
 
-name = rearrange_names("Kennedy, John F.")
-print(name)
+
+print(re.search(r"^([A-Z]\w*),\s([A-Z][\w\.\-]*)\s([A-Z][\w\.\-]*)$",  "Kennedy, John Franklin"))
+
 
