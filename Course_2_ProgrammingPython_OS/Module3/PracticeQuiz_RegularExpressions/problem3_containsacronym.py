0f8f1a1fc98c2adc516ccb3394fd834aa36b7ecd
cirby1 <calvin.irby01@gmail.com>
2025-12-18 21:49:56 -0600

    -

--- Course_2_ProgrammingPython_OS/Module3/PracticeQuiz_RegularExpressions/problem3_containsacronym.py
+++ Course_2_ProgrammingPython_OS/Module3/PracticeQuiz_RegularExpressions/problem3_containsacronym.py
@@ -1,6 +1,4 @@
 #3 - The contains_acronym() function checks the text for the presence of 2 or more characters or digits surrounded by parenthesis, with at least the first character in uppercase(if it's a letter), returning True if the condition is met, or False otherwise. For example, "Instant messaging (IM) is a set of communication technologies used for text-based communication" should return True since (IM) satisfies the match conditions. Fill in the regular expression in the function.
-
-
 #Parenthesis with at least 2 or more characters or digits within the parenthesis. With at least the first character being uppercase(if it's a letter)
 
 import re
